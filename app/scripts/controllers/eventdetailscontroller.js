3f24de7ac7081fe68fbdd9c07ed248310d43a23a
suraj.jh <suraj.jh@directi.com>
2015-02-26 11:38:51 +0530

    done with initial setup. added all contr views etc and updated app.js routes

--- app/scripts/controllers/eventdetailscontroller.js
+++ app/scripts/controllers/eventdetailscontroller.js
@@ -13,18 +13,42 @@ angular.module('rollApp')
         $scope.myInterval = 3000;
         $scope.slides = [
             {
-                image: 'http://lorempixel.com/400/200/'
+                image: 'http://lorempixel.com/1000/400/'
             },
             {
-                image: 'http://lorempixel.com/400/200/food'
+                image: 'http://lorempixel.com/1000/400/food'
             },
             {
-                image: 'http://lorempixel.com/400/200/sports'
+                image: 'http://lorempixel.com/1000/400/sports'
             },
             {
-                image: 'http://lorempixel.com/400/200/people'
+                image: 'http://lorempixel.com/1000/400/people'
             }
         ];
+
+        $scope.mapOptions = {
+            center: new google.maps.LatLng(19.121, 72.85),
+            zoom:16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        //var map = new google.maps.Map(document.getElementById('myMap'), $scope.mapOptions);
+
+        $scope.codeAddress = (function(){
+            var geocoder = new google.maps.Geocoder();
+            //var address = $scope.address;
+            var address = "andheri";
+            geocoder.geocode( { 'address': address}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    $scope.myMap.setCenter(results[0].geometry.location);
+                    var marker = new google.maps.Marker({
+                        map: $scope.myMap,
+                        position: results[0].geometry.location
+                    });
+                } else {
+                    alert("Geocode was not successful for the following reason: " + status);
+                }
+            });
+        })();
 
   });
 
