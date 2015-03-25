59e7d7c852612a814c03485fb8259a8e28bfe821
suraj.jh <suraj.jh@directi.com>
2015-03-25 13:05:58 +0530

    done with home page

--- app/scripts/directives/navbar.js
+++ app/scripts/directives/navbar.js
@@ -9,7 +9,7 @@
 angular.module('rollApp')
   .directive('navbar', function ($rootScope) {
     return {
-      templateUrl: $rootScope.baseUrl+'/app/views/partials/navbar.html',
+      templateUrl: 'views/partials/navbar.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
        //     //console.log($("angucomplete-alt"))
